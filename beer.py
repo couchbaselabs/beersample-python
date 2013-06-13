22bf13e7f0d12a94f2136e3aea2d998b8860e15e
Mark Nunberg <mnunberg@haskalah.org>
2013-06-13 13:12:58 -0700

    merge beerobjs and beer

--- beer.py
+++ beer.py
@@ -1,18 +1,67 @@
 from collections import namedtuple
 import json
 
-from flask import Flask, request, session, g, redirect, url_for, \
-        abort, render_template, flash, Response
+from flask import Flask, request, redirect, abort, render_template
 
 from couchbase import Couchbase
 from couchbase.exceptions import KeyExistsError
 from couchbase.views.iterator import RowProcessor
 from couchbase.views.params import UNSPEC, Query
 
-from beerobjs import BeerListRowProcessor, Beer
-
 
 BreweryRow = namedtuple('BreweryRow', ['name', 'value', 'id', 'doc'])
+
+class Beer(object):
+    def __init__(self, id, name, doc=None):
+        self.id = id
+        self.name = name
+        self.brewery = None
+        self.doc = doc
+
+    def __getattr__(self, name):
+        if not self.doc:
+            return ""
+        return self.doc.get(name, "")
+
+
+class BeerListRowProcessor(object):
+    """
+    This is the row processor for listing all beers (with their brewery IDs).
+    """
+    def __init__(self):
+        self.iterator = None
+
+    def handle_rows(self, rows, connection, include_docs):
+        ret = []
+        by_docids = {}
+
+        for r in rows:
+            b = Beer(r['id'], r['key'])
+            ret.append(b)
+            by_docids[b.id] = b
+
+        keys_to_fetch = [ x.id for x in ret ]
+        docs = connection.get_multi(keys_to_fetch, quiet=True)
+
+        for beer_id, doc in docs.items():
+            if not doc.success:
+                ret.remove(beer)
+                continue
+
+            beer = by_docids[beer_id]
+            beer.brewery_id = doc.value['brewery_id']
+
+        self.iterator = iter(ret)
+
+    def __iter__(self):
+        if not self.iterator:
+            return
+
+        for beer in self.iterator:
+            yield beer
+
+        self.iterator = None
+
 
 DATABASE = 'beer-sample'
 HOST = 'localhost'
